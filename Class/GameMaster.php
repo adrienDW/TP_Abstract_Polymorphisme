cb17b351d04f128468f5dfa09f5c5ca2e52a8ef2
adrienDW <105793548+adrienDW@users.noreply.github.com>
2023-11-02 16:52:15 +0100

    Add files via upload

--- Class/GameMaster.php
+++ Class/GameMaster.php
@@ -4,8 +4,7 @@
     require_once './Class/Dice.php';
     require_once './Class/GameValue.php';
 class gameMaster{
-    public function __construct( protected Deck $deck1, protected Deck $deck2, 
-    protected Coin $coin1, protected Coin $coin2, protected Dice $dice1, protected Dice $dice2)
+    public function __construct( protected RandomGenerator $gen)
     {
         
     }
